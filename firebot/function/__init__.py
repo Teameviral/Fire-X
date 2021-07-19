6d048666351793736635c738bea6e1e84f139978
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-07-20 00:04:53 +0100

    Update __init__.py

--- firebot/function/__init__.py
+++ firebot/function/__init__.py
@@ -500,7 +500,7 @@ async def check_if_subbed(channel_id, event, bot):
 
 async def _ytdl(url, is_it, event, tgbot):
     await event.edit(
-        "`Ok Downloading This Video / Audio - Please Wait.` \n**Powered By @VirtualUserbot**"
+        "`Ok Downloading This Video / Audio - Please Wait.` \n**Powered By Firebot**"
     )
     if is_it:
         opts = {
@@ -592,14 +592,14 @@ async def _ytdl(url, is_it, event, tgbot):
             ),
         )
         await event.edit(
-            file=hmmo, text=f"{ytdl_data['title']} \n**Uploaded Using @VirtualUserbot**"
+            file=hmmo, text=f"{ytdl_data['title']} \n**Uploaded Using Firebot**"
         )
         os.remove(f"{ytdl_data['id']}.mp4")
 
 
 async def _deezer_dl(word, event, tgbot):
     await event.edit(
-        "`Ok Downloading This Audio - Please Wait.` \n**Powered By @VirtualUserbot**"
+        "`Ok Downloading This Audio - Please Wait.` \n**Powered By firebot**"
     )
     link = f"https://api.deezer.com/search?q={word}&limit=1"
     dato = requests.get(url=link).json()
@@ -635,7 +635,7 @@ Get Your firebot From Fire-X"""
 
     await event.edit(
         file=uploaded_file,
-        text=f"""{urlhp.get("title")} \n**Uploaded Using @VirtualUserbot**""",
+        text=f"""{urlhp.get("title")} \n**Uploaded Using Firebot**""",
     )
     os.remove(sname)
 
