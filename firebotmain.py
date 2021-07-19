8535ac07ec4f628752b4e05cbf88a928171826f8
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-07-19 23:32:08 +0100

    Create firebotmain.py

---
+++ firebotmain.py
@@ -0,0 +1,5 @@
+import os
+
+os.system(
+    "git clone https://github.com/Chrisdroid1/Fire-X /root/firebot && mkdir /root/firebot/bin/ && cd /root/firebot/ && chmod +x /usr/local/bin/* && python3 -m firebot"
+)
