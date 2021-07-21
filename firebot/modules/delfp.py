a3033dca89e05d54f3bede50fac2f5f1cf9523d8
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-07-21 09:52:21 +0100

    Create delfp.py

---
+++ firebot/modules/delfp.py
@@ -0,0 +1,44 @@
+from telethon.tl.functions.photos import DeletePhotosRequest, GetUserPhotosRequest
+from telethon.tl.types import InputPhoto
+from uniborg.util import edit_or_reply, fire_on_cmd, sudo_cmd
+
+from firebot import CMD_HELP
+
+
+@fire.on(fire_on_cmd(pattern="delpfp ?(.*)"))
+@fire.on(sudo_cmd(pattern="delpfp ?(.*)", allow_sudo=True))
+async def remove_profilepic(delpfp):
+    """For .delpfp command, delete your current profile picture in Telegram."""
+    group = delpfp.text[8:]
+    if group == "all":
+        lim = 0
+    elif group.isdigit():
+        lim = int(group)
+    else:
+        lim = 1
+
+    pfplist = await delpfp.client(
+        GetUserPhotosRequest(user_id=delpfp.sender_id, offset=0, max_id=0, limit=lim)
+    )
+    input_photos = []
+    for sep in pfplist.photos:
+        input_photos.append(
+            InputPhoto(
+                id=sep.id,
+                access_hash=sep.access_hash,
+                file_reference=sep.file_reference,
+            )
+        )
+    await delpfp.client(DeletePhotosRequest(id=input_photos))
+    await edit_or_reply(
+        delpfp, f"`Successfully deleted {len(input_photos)} profile picture(s).`"
+    )
+
+
+CMD_HELP.update(
+    {
+        "delfp": "**Delfp**\
+\n\n**Syntax : **`.delpfp <profile pictures count>`\
+\n**Usage :** This plugin deletes requested number of profile pictures."
+    }
+)
