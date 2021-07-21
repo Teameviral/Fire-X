aaffc5a9b791e7591a350f97d9637773f4bc2b64
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-07-21 07:36:35 +0100

    Create instant_install_ext_module.py

---
+++ firebot/modules/instant_install_ext_module.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+
+from telethon.tl.types import InputMessagesFilterDocument
+
+from var import Var
+from firebot.utils import command, load_module
+
+
+@command(pattern="^.extdl", outgoing=True)
+async def install(event):
+    if event.fwd_from:
+        return
+    chat = Var.PLUGIN_CHANNEL
+    documentss = await borg.get_messages(chat, None, filter=InputMessagesFilterDocument)
+    total = int(documentss.total)
+    total_doxx = range(0, total)
+    await event.delete()
+    for ixo in total_doxx:
+        mxo = documentss[ixo].id
+        downloaded_file_name = await event.client.download_media(
+            await borg.get_messages(chat, ids=mxo), "virtualuserbot/modules/"
+        )
+        if "(" not in downloaded_file_name:
+            path1 = Path(downloaded_file_name)
+            shortname = path1.stem
+            load_module(shortname.replace(".py", ""))
+            await borg.send_message(
+                event.chat_id,
+                "Installed Plugin `{}` successfully.".format(
+                    os.path.basename(downloaded_file_name)
+                ),
+            )
+        else:
+            await borg.send_message(
+                event.chat_id,
+                "Plugin `{}` has been pre-installed and cannot be installed.".format(
+                    os.path.basename(downloaded_file_name)
+                ),
+            )
