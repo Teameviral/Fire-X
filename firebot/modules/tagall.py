c07ef875dc8452036e63671d2cb3a8764717c0c3
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-07-21 12:43:30 +0100

    Update tagall.py

--- firebot/modules/tagall.py
+++ firebot/modules/tagall.py
@@ -1,7 +1,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from uniborg.util import friday_on_cmd
+from uniborg.util import fire_on_cmd
 
 from fireboy import CMD_HELP
 
