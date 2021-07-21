a532d46e1fc010147b7a2138c685c80cb49995df
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-07-21 08:43:30 +0100

    Create copy.py

---
+++ firebot/modules/copy.py
@@ -0,0 +1,21 @@
+from firebot import CMD_HELP
+
+from ..utils import admin_cmd
+
+
+@borg.on(admin_cmd(pattern="copy"))
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        the_real_message = previous_message.text
+        event.reply_to_msg_id
+        the_real_message = the_real_message.replace("*", "*")
+        the_real_message = the_real_message.replace("_", "_")
+        await event.edit(the_real_message)
+    else:
+        await event.edit("Reply to a  message .copy to copy nd paste ")
+
+
+CMD_HELP.update({"copy": ".c <reply to any text> " "\nc that text nd redeliver it"})
