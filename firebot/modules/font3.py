a363dcc7c51c65bf2410a432e49bafde58acd282
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-07-21 11:20:49 +0100

    Create font3.py

---
+++ firebot/modules/font3.py
@@ -0,0 +1,207 @@
+from firebot import CMD_HELP
+from firebot.modules import fonts
+
+from ..utils import admin_cmd, edit_or_reply, sudo_cmd
+
+
+@bot.on(admin_cmd(pattern="fmusical(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="fmusical(?: |$)(.*)", allow_sudo=True))
+async def stylish_generator(event):
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "What I am Supposed to change give text")
+        return
+    string = "  ".join(args).lower()
+    for normalfontcharacter in string:
+        if normalfontcharacter in fonts.normalfont:
+            musicalcharacter = fonts.musicalfont[
+                fonts.normalfont.index(normalfontcharacter)
+            ]
+            string = string.replace(normalfontcharacter, musicalcharacter)
+    await edit_or_reply(event, string)
+
+
+@bot.on(admin_cmd(pattern="ancient(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="ancient(?: |$)(.*)", allow_sudo=True))
+async def stylish_generator(event):
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "What I am Supposed to change give text")
+        return
+    string = "  ".join(args).lower()
+    for normalfontcharacter in string:
+        if normalfontcharacter in fonts.normalfont:
+            ancientcharacter = fonts.ancientfont[
+                fonts.normalfont.index(normalfontcharacter)
+            ]
+            string = string.replace(normalfontcharacter, ancientcharacter)
+    await edit_or_reply(event, string)
+
+
+@bot.on(admin_cmd(pattern="vapor(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="vapor(?: |$)(.*)", allow_sudo=True))
+async def vapor(vpr):
+    reply_text = []
+    textx = await vpr.get_reply_message()
+    message = vpr.pattern_match.group(1)
+    if message:
+        pass
+    elif textx:
+        message = textx.text
+    else:
+        await edit_or_reply(vpr, "`Ｇｉｖｅ ｓｏｍｅ ｔｅｘｔ ｆｏｒ ｖａｐｏｒ！`")
+        return
+
+    for charac in message:
+        if 0x21 <= ord(charac) <= 0x7F:
+            reply_text.append(chr(ord(charac) + 0xFEE0))
+        elif ord(charac) == 0x20:
+            reply_text.append(chr(0x3000))
+        else:
+            reply_text.append(charac)
+
+    await edit_or_reply(vpr, "".join(reply_text))
+
+
+@bot.on(admin_cmd(pattern="smallcaps(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="smallcaps(?: |$)(.*)", allow_sudo=True))
+async def stylish_generator(event):
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "What I am Supposed to change give text")
+        return
+    string = "  ".join(args).lower()
+    for normaltextcharacter in string:
+        if normaltextcharacter in fonts.normaltext:
+            smallcapscharacter = fonts.smallcapsfont[
+                fonts.normaltext.index(normaltextcharacter)
+            ]
+            string = string.replace(normaltextcharacter, smallcapscharacter)
+    await edit_or_reply(event, string)
+
+
+@bot.on(admin_cmd(pattern="blackbf(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="blackbf(?: |$)(.*)", allow_sudo=True))
+async def stylish_generator(event):
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "What I am Supposed to change give text")
+        return
+    string = "  ".join(args).lower()
+    for normaltextcharacter in string:
+        if normaltextcharacter in fonts.normaltext:
+            bubblesblackcharacter = fonts.bubblesblackfont[
+                fonts.normaltext.index(normaltextcharacter)
+            ]
+            string = string.replace(normaltextcharacter, bubblesblackcharacter)
+    await edit_or_reply(event, string)
+
+
+@bot.on(admin_cmd(pattern="bubbles(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="bubbles(?: |$)(.*)", allow_sudo=True))
+async def stylish_generator(event):
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "What I am Supposed to change give text")
+        return
+    string = "  ".join(args).lower()
+    for normaltextcharacter in string:
+        if normaltextcharacter in fonts.normaltext:
+            bubblescharacter = fonts.bubblesfont[
+                fonts.normaltext.index(normaltextcharacter)
+            ]
+            string = string.replace(normaltextcharacter, bubblescharacter)
+    await edit_or_reply(event, string)
+
+
+@bot.on(admin_cmd(pattern="tanf(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="tanf(?: |$)(.*)", allow_sudo=True))
+async def stylish_generator(event):
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "What I am Supposed to change give text")
+        return
+    string = "  ".join(args).lower()
+    for normaltextcharacter in string:
+        if normaltextcharacter in fonts.normaltext:
+            tantextcharacter = fonts.tantextfont[
+                fonts.normaltext.index(normaltextcharacter)
+            ]
+            string = string.replace(normaltextcharacter, tantextcharacter)
+    await edit_or_reply(event, string)
+
+
+@bot.on(admin_cmd(pattern="boxf(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="boxf(?: |$)(.*)", allow_sudo=True))
+async def stylish_generator(event):
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "What I am Supposed to change give text")
+        return
+    string = "  ".join(args).lower()
+    for normaltextcharacter in string:
+        if normaltextcharacter in fonts.normaltext:
+            littleboxtextcharacter = fonts.littleboxtextfont[
+                fonts.normaltext.index(normaltextcharacter)
+            ]
+            string = string.replace(normaltextcharacter, littleboxtextcharacter)
+    await edit_or_reply(event, string)
+
+
+@bot.on(admin_cmd(pattern="smothtext(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="smothtext(?: |$)(.*)", allow_sudo=True))
+async def stylish_generator(event):
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "What I am Supposed to change give text")
+        return
+    string = "  ".join(args).lower()
+    for normaltextcharacter in string:
+        if normaltextcharacter in fonts.normaltext:
+            smothtextcharacter = fonts.smothtextfont[
+                fonts.normaltext.index(normaltextcharacter)
+            ]
+            string = string.replace(normaltextcharacter, smothtextcharacter)
+    await edit_or_reply(event, string)
+
+
+CMD_HELP.update(
+    {
+        "fonts": """**Plugin : **`fonts`
+**Commands found in fonts are**
+  •  `.fmusical`
+  •  `.ancient`
+  •  `.vapor`
+  •  `.smallcaps`
+  •  `.blackbf`
+  •  `.bubbles`
+  •  `.tanf`
+  •  `.boxf`
+  •  `.smothtext`
+**Function : **__Reply the command to the text message or give input along with command to convert that text to given font style__"""
+    }
+)
