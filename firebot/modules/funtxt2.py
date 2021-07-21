49b5f9d92296ba60d3aa7762bfada99978fa34b2
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-07-21 11:44:50 +0100

    Create funtxt2.py

---
+++ firebot/modules/funtxt2.py
@@ -0,0 +1,45 @@
+import nekos
+
+from firebot import CMD_HELP
+
+from ..utils import admin_cmd, edit_or_reply, sudo_cmd
+
+
+@bot.on(admin_cmd(pattern="tcat$"))
+@bot.on(sudo_cmd(pattern="tcat$", allow_sudo=True))
+async def hmm(cat):
+    if cat.fwd_from:
+        return
+    reactcat = nekos.textcat()
+    await edit_or_reply(cat, reactcat)
+
+
+@bot.on(admin_cmd(pattern="why$"))
+@bot.on(sudo_cmd(pattern="why$", allow_sudo=True))
+async def hmm(cat):
+    if cat.fwd_from:
+        return
+    whycat = nekos.why()
+    await edit_or_reply(cat, whycat)
+
+
+@bot.on(admin_cmd(pattern="fact$"))
+@bot.on(sudo_cmd(pattern="fact$", allow_sudo=True))
+async def hmm(cat):
+    if cat.fwd_from:
+        return
+    factcat = nekos.fact()
+    await edit_or_reply(cat, factcat)
+
+
+CMD_HELP.update(
+    {
+        "funtxts": """**Plugin : **`funtxts`
+  •  **Syntax : **`.tcat`
+  •  **Function : **__Sens you some random cat facial text art__
+  •  **Syntax : **`.why`
+  •  **Function : **__Asks some random Funny questions__
+  •  **Syntax : **`.fact`
+  •  **Function : **__Sends you some random facts__"""
+    }
+)
