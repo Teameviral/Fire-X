4b8e2658fd04a99c556324f7c0a945ab71cc0416
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-07-21 12:49:03 +0100

    Create gangasta.py

---
+++ firebot/modules/gangasta.py
@@ -0,0 +1,33 @@
+import asyncio
+
+from firebot import CMD_HELP
+from firebot.utils import fire_on_cmd
+
+
+@fire.on(fire_on_cmd("gangasta ?(.*)"))
+async def _(event):
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("EVERyBOdy")
+        await asyncio.sleep(0.3)
+        await event.edit("iZ")
+        await asyncio.sleep(0.2)
+        await event.edit("GangSTur")
+        await asyncio.sleep(0.5)
+        await event.edit("UNtIL ")
+        await asyncio.sleep(0.2)
+        await event.edit("I")
+        await asyncio.sleep(0.3)
+        await event.edit("ArRivE")
+        await asyncio.sleep(0.3)
+        await event.edit("🔥🔥🔥")
+        await asyncio.sleep(0.3)
+        await event.edit("EVERyBOdy iZ GangSTur UNtIL I ArRivE 🔥🔥🔥")
+
+
+CMD_HELP.update(
+    {
+        "gangasta": "**Gangasta**\
+\n\n**Syntax : **`.gangasta`\
+\n**Usage :** shows your gangster skills."
+    }
+)
