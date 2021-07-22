9ba9d94c37d5748d9c99dc5625557a223bd7c44c
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-07-22 16:46:31 +0100

    Create jio.py

---
+++ firebot/modules/jio.py
@@ -0,0 +1,60 @@
+import asyncio
+
+from uniborg.util import fire_on_cmd
+
+from firebot import CMD_HELP
+
+
+@fire.on(fire_on_cmd(pattern=r"jio"))
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 1
+
+    animation_ttl = range(0, 19)
+
+    # input_str = event.pattern_match.group(1)
+
+    # if input_str == "jio":
+
+    await event.edit("jio")
+
+    animation_chars = [
+        "`Connecting To JIO Network 📡 ....`",
+        "`█ ▇ ▆ ▅ ▄ ▂ ▁`",
+        "`▒ ▇ ▆ ▅ ▄ ▂ ▁`",
+        "`▒ ▒ ▆ ▅ ▄ ▂ ▁`",
+        "`▒ ▒ ▒ ▅ ▄ ▂ ▁`",
+        "`▒ ▒ ▒ ▒ ▄ ▂ ▁`",
+        "`▒ ▒ ▒ ▒ ▒ ▂ ▁`",
+        "`▒ ▒ ▒ ▒ ▒ ▒ ▁`",
+        "`▒ ▒ ▒ ▒ ▒ ▒ ▒`",
+        "*Optimising Network....*",
+        "`▒ ▒ ▒ ▒ ▒ ▒ ▒`",
+        "`▁ ▒ ▒ ▒ ▒ ▒ ▒`",
+        "`▁ ▂ ▒ ▒ ▒ ▒ ▒`",
+        "`▁ ▂ ▄ ▒ ▒ ▒ ▒`",
+        "`▁ ▂ ▄ ▅ ▒ ▒ ▒`",
+        "`▁ ▂ ▄ ▅ ▆ ▒ ▒`",
+        "`▁ ▂ ▄ ▅ ▆ ▇ ▒`",
+        "`▁ ▂ ▄ ▅ ▆ ▇ █`",
+        "**JIO Network Connected and Boosted....**",
+    ]
+
+    for i in animation_ttl:
+
+        await asyncio.sleep(animation_interval)
+
+        await event.edit(animation_chars[i % 19])
+
+
+CMD_HELP.update(
+    {
+        "jio": "**Jio**\
+\n\n**Syntax : **`.jio`\
+\n**Usage :** funny prank plugin."
+    }
+)
