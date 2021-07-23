fe7ecee258ed201df0a29ea0a64e8c85fd3d53a4
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-07-23 21:06:10 +0100

    Create send.py

---
+++ firebot/modules/send.py
@@ -0,0 +1,41 @@
+import asyncio
+from datetime import datetime
+
+from firebot import CMD_HELP
+from firebot.utils import fire_on_cmd, sudo_cmd
+
+firethumb = "./resources/IMG_20210719_203716_508.jpg"
+
+
+@friday.on(friday_on_cmd(pattern="send ?(.*)"))
+@friday.on(sudo_cmd(pattern="send ?(.*)", allow_sudo=True))
+async def send(event):
+    if event.fwd_from:
+        return
+    message_id = event.message.id
+    input_str = event.pattern_match.group(1)
+    start = datetime.now()
+    the_plugin_file = "./firebot/modules/{}.py".format(input_str)
+    end = datetime.now()
+    (end - start).seconds
+    men = f"Plugin Name - {input_str}.py \nUploaded By Fire-X"
+    await event.client.send_file(  # pylint:disable=E0602
+        event.chat_id,
+        the_plugin_file,
+        thumb=firethumb,
+        caption=men,
+        force_document=True,
+        allow_cache=False,
+        reply_to=message_id,
+    )
+    await asyncio.sleep(5)
+    await event.delete()
+
+
+CMD_HELP.update(
+    {
+        "send": "**Send**\
+\n\n**Syntax : **`.send <plugin name>`\
+\n**Usage :** sends the plugin."
+    }
+)
