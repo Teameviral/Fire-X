e331874b591b8efef328475a8db16a317e557561
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-07-24 07:02:33 +0100

    Update __init__.py

--- firebot/modules/sql_helper/__init__.py
+++ firebot/modules/sql_helper/__init__.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 # the secret configuration specific things
-from firebot.var import Var
+from var import Var
 
 
 def start() -> scoped_session:
