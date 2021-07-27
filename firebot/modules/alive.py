b4600c683e99ec325021ffd0471037074a021768
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-07-27 06:32:45 +0100

    Update alive.py

--- firebot/modules/alive.py
+++ firebot/modules/alive.py
@@ -7,7 +7,37 @@ from firebot.utils import admin_cmd, sudo_cmd
 from telethon.tl.types import ChannelParticipantsAdmins
 from firebot import CMD_HELP
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "firebot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+
+uptime = get_readable_time((time.time() - Lastupdate))
 
 chrissudo = Config.SUDO_USERS
 
