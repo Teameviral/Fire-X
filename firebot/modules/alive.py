241b5b2d3a7916c357377135a1f44c0b5f670ab9
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-07-27 05:43:28 +0100

    Update alive.py

--- firebot/modules/alive.py
+++ firebot/modules/alive.py
@@ -1,47 +1,31 @@
-"""Check if Fire-X alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
-# CREDITS: @WhySooSerious, @Sur_vivor
-import time
+import asyncio
+import random
+from telethon import events, version
+from userbot import ALIVE_NAME, currentversion
+from userbot.utils import admin_cmd, sudo_cmd
+from telethon.tl.types import ChannelParticipantsAdmins
+from firebot import CMD_HELP
 
-from uniborg.util import fire_on_cmd, sudo_cmd
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "firebot"
 
-from firebot import ALIVE_NAME, CMD_HELP, Lastupdate
-from firebot.Configs import Config
-from firebot.modules import currentversion
-from telethon import version
+chrissudo = Config.SUDO_USERS
 
-
-# Functions
-def get_readable_time(seconds: int) -> str:
-    count = 0
-    ping_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
-
-    while count < 4:
-        count += 1
-        if count < 3:
-            remainder, result = divmod(seconds, 60)
-        else:
-            remainder, result = divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        ping_time += time_list.pop() + ", "
-
-    time_list.reverse()
-    ping_time += ":".join(time_list)
-
-    return ping_time
+if chrissudo:
+    sudou = "True"
+else:
+    sudou = "False"
 
 
-uptime = get_readable_time((time.time() - Lastupdate))
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Fire-X"
-PM_IMG = Config.ALIVE_IMAGE
+edit_time = 16
+""" =======================CONSTANTS====================== """
+file1 = "https://telegra.ph/file/f36754b7d29242bd7b15c.jpg"
+file2 = "https://telegra.ph/file/9c135ed934dd65bff97ac.jpg"
+file3 = "https://telegra.ph/file/9ab1513be4a75fa59dd12.jpg"
+file4 = "https://telegra.ph/file/9ab1513be4a75fa59dd12.jpg"
+""" =======================CONSTANTS====================== """
+
+
+
 pm_caption = "  __**★ғɪʀᴇ-x ɪs ʀᴜɴɴɪɴɢ sᴜᴄᴇssғᴜʟʟʏ★**__\n\n"
 
 pm_caption += f"**━━━━━━━|━━━━━|━━━━━━**\n\n"
@@ -57,13 +41,58 @@ pm_caption += f"┗━━━━━━━━━━━━━━━━━━━\n"
 pm_caption += f" ||•|| sᴇᴄᴜʀɪᴛʏ ʙʏ ғɪʀᴇ-x ||•||\n"
 pm_caption += " [ɢɪᴛʜᴜʙ](https://github.com/Chrisdroid1/Fire-X) • [ɢʀᴏᴜᴘ](https://t.me/FireXUserBot)"
 
+# @command(outgoing=True, pattern="^.alive$")
+@bot.on(admin_cmd(outgoing=True, pattern="alive$"))
+@bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
+async def amireallyalive(alive):
+    await alive.get_chat()   
+    await alive.delete()
+    on = await borg.send_file(alive.chat_id, file=file1,caption=pm_caption)
 
-@fire.on(fire_on_cmd(pattern=r"alive"))
-@fire.on(sudo_cmd(pattern=r"alive", allow_sudo=True))
-async def chris(alive):
-    await alive.get_chat()
+    await asyncio.sleep(edit_time)
+    ok = await borg.edit_message(alive.chat_id, on, file=file2) 
+
+    await asyncio.sleep(edit_time)
+    ok2 = await borg.edit_message(alive.chat_id, ok, file=file3)
+
+    await asyncio.sleep(edit_time)
+    ok3 = await borg.edit_message(alive.chat_id, ok2, file=file1)
+    
+    await asyncio.sleep(edit_time)
+    ok4 = await borg.edit_message(alive.chat_id, ok3, file=file3)
+    
+    await asyncio.sleep(edit_time)
+    ok5 = await borg.edit_message(alive.chat_id, ok4, file=file2)
+    
+    await asyncio.sleep(edit_time)
+    ok6 = await borg.edit_message(alive.chat_id, ok5, file=file4)
+    
+    await asyncio.sleep(edit_time)
+    ok7 = await borg.edit_message(alive.chat_id, ok6, file=file1)
+    
+    await asyncio.sleep(edit_time)
+    ok8 = await borg.edit_message(alive.chat_id, ok7, file=file2) 
+
+    await asyncio.sleep(edit_time)
+    ok9 = await borg.edit_message(alive.chat_id, ok8, file=file3)
+
+    await asyncio.sleep(edit_time)
+    ok10 = await borg.edit_message(alive.chat_id, ok9, file=file1)
+    
+    await asyncio.sleep(edit_time)
+    ok11 = await borg.edit_message(alive.chat_id, ok10, file=file3)
+    
+    await asyncio.sleep(edit_time)
+    ok12 = await borg.edit_message(alive.chat_id, ok11, file=file2)
+    
+    await asyncio.sleep(edit_time)
+    ok13 = await borg.edit_message(alive.chat_id, ok12, file=file4)
+    
+    await asyncio.sleep(edit_time)
+    ok14 = await borg.edit_message(alive.chat_id, on, file=file1)
+
     """ For .alive command, check if the bot is running.  """
-    await borg.send_file(alive.chat_id, PM_IMG, caption=pm_caption)
+    await borg.send_file(alive.chat_id, caption=pm_caption)
     await alive.delete()
 
 
