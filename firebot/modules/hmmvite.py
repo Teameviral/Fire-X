e598b854e71bc70dd055dbf104d552352a01a895
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-08-08 09:34:20 +0100

    Update hmmvite.py

--- firebot/modules/hmmvite.py
+++ firebot/modules/hmmvite.py
@@ -6,8 +6,10 @@ from telethon.errors import (
 from telethon.tl import functions
 from telethon.tl.functions.channels import GetFullChannelRequest
 from telethon.tl.functions.messages import GetFullChatRequest
-from firebot.utils import admin_cmd, edit_or_reply, sudo_cmd
-from firebot import CMD_HELP
+
+from firebot import *
+from firebot.utils import fire_on_cmd
+
 
 async def get_chatinfo(event):
     chat = event.pattern_match.group(1)
@@ -46,6 +48,18 @@ async def get_chatinfo(event):
     return chat_info
 
 
+def make_mention(user):
+    if user.username:
+        return f"@{user.username}"
+    else:
+        return inline_mention(user)
+
+
+def inline_mention(user):
+    full_name = user_full_name(user) or "No Name"
+    return f"[{full_name}](tg://user?id={user.id})"
+
+
 def user_full_name(user):
     names = [user.first_name, user.last_name]
     names = [i for i in list(names) if i]
@@ -53,79 +67,39 @@ def user_full_name(user):
     return full_name
 
 
-@bot.on(admin_cmd(pattern="kidnappall ?(.*)"))
-@bot.on(sudo_cmd(pattern="kidnappall ?(.*)", allow_sudo=True))
+@borg.on(fire_on_cmd(pattern=r"allinvite ?(.*)"))
 async def get_users(event):
     sender = await event.get_sender()
     me = await event.client.get_me()
     if not sender.id == me.id:
-        hell = await edit_or_reply(event, "`processing to kidnapp...`")
+        rkp = await event.reply("`processing...`")
     else:
-        hell = await edit_or_reply(event, "`processing to kidnap...`")
-    kraken = await get_chatinfo(event)
+        rkp = await event.edit("`processing...`")
+    rk1 = await get_chatinfo(event)
     chat = await event.get_chat()
     if event.is_private:
-        return await hell.edit("`Sorry, kidnapp users here`")
+        return await rkp.edit("`Sorry, Can add users here`")
     s = 0
     f = 0
     error = "None"
 
-    await hell.edit("♤KidnappedStatus♤\n\n`Kidnaping Users.......`")
-    async for user in event.client.iter_participants(kraken.full_chat.id):
+    await rkp.edit("**TerminalStatus**\n\n`Collecting Users.......`")
+    async for user in event.client.iter_participants(rk1.full_chat.id):
         try:
             if error.startswith("Too"):
-                return await hell.edit(
-                    f"**Kidnapping Finished With Error**\n(`May Got Limit Error from kidnaper lol, Please try again Later`)\n**Error** : \n`{error}`\n\n• Kidnapped `{s}` people \n• Failed to Kidnapp `{f}` people"
+                return await rkp.edit(
+                    f"**Terminal Finished With Error**\n(`May Got Limit Error from telethon Please try agin Later`)\n**Error** : \n`{error}`\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people"
                 )
             await event.client(
                 functions.channels.InviteToChannelRequest(channel=chat, users=[user.id])
             )
             s = s + 1
-            await hell.edit(
-                f"**Kidnapping Running...**\n\n• Kidnapped `{s}` people \n• Failed to Kidnapp `{f}` people\n\n**× LastError:** `{error}`"
+            await rkp.edit(
+                f"**Terminal Running...**\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people\n\n**× LastError:** `{error}`"
             )
         except Exception as e:
             error = str(e)
             f = f + 1
-    return await hell.edit(
-        f"**Kidnapping Finished** \n\n• Successfully Kidnapped `{s}` people \n• failed to Kidnapp `{f}` people"
+    return await rkp.edit(
+        f"**Terminal Finished** \n\n• Successfully Invited `{s}` people \n• failed to invite `{f}` people"
     )
-
-
-@bot.on(admin_cmd(pattern="kidnapp ?(.*)"))
-@bot.on(sudo_cmd(pattern="kidnapp ?(.*)", allow_sudo=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    to_add_users = event.pattern_match.group(1)
-    if event.is_private:
-        await edit_or_reply(
-            event, "`.kidnapp` users to a chat, not to a Private Message"
-        )
-    else:
-        logger.info(to_add_users)
-        if not event.is_channel and event.is_group:
-            # https://lonamiwebs.github.io/Telethon/methods/messages/add_chat_user.html
-            for user_id in to_add_users.split(" "):
-                try:
-                    await borg(
-                        functions.messages.AddChatUserRequest(
-                            chat_id=event.chat_id, user_id=user_id, fwd_limit=1000000
-                        )
-                    )
-                except Exception as e:
-                    await event.reply(str(e))
-            await edit_or_reply(event, "kidnapped Successfully")
-        else:
-            # https://lonamiwebs.github.io/Telethon/methods/channels/invite_to_channel.html
-            for user_id in to_add_users.split(" "):
-                try:
-                    await borg(
-                        functions.channels.InviteToChannelRequest(
-                            channel=event.chat_id, users=[user_id]
-                        )
-                    )
-                except Exception as e:
-                    await event.reply(str(e))
-            await edit_or_reply(event, "kidnapped user to the chat....")
-            
