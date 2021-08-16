3618fb2ca706a56843d0895211b6a28a23e2c15d
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-08-16 18:46:11 +0100

    Update compress.py

--- firebot/modules/compress.py
+++ firebot/modules/compress.py
@@ -44,7 +44,7 @@ async def _(event):
             await borg.send_file(
                 event.chat_id,
                 directory_name + ".zip",
-                caption="Zipped By [𝔅𝔩𝔞𝔠𝔨 𝔏𝔦𝔤𝔥𝔱𝔫𝔦𝔫𝔤](https://github.com/Anmol-dot283/Black-Lightning)",
+                caption="Zipped By [Fire-X](https://github.com/TeamEviral/Fire-X)",
                 force_document=True,
                 allow_cache=False,
                 reply_to=event.message.id,
