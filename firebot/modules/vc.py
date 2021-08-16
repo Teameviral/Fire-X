0fae360f0020e7f7dfec0194c6f52de86df89e02
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-08-16 23:23:42 +0100

    Update vc.py

--- firebot/modules/vc.py
+++ firebot/modules/vc.py
@@ -1,33 +1,56 @@
-from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
-from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
+from telethon.tl.functions.channels import GetFullChannelRequest
+from telethon.tl.functions.phone import CreateGroupCallRequest
+from telethon.tl.functions.phone import DiscardGroupCallRequest
+from telethon.tl.functions.phone import GetGroupCallRequest
+from telethon.tl.functions.phone import InviteToGroupCallRequest
 
-from firebot.events import register
-
-
-async def get_call(event):
-    mm = await event.client(getchat(event.chat_id))
-    xx = await event.client(getvc(mm.full_chat.call))
-    return xx.call
+from . import *
 
 
-def user_list(l, n):
-    for i in range(0, len(l), n):
-        yield l[i : i + n]
+async def getvc(event):
+    chat_ = await event.client(GetFullChannelRequest(event.chat_id))
+    _chat = await event.client(GetGroupCallRequest(chat_.full_chat.call))
+    return _chat.call
+
+def all_users(a, b):
+    for c in range(0, len(a), b):
+        yield a[c : c + b]
 
 
-@register(outgoing=True, pattern=r"^\.joinvc (.*)")
+@bot.on(fire_on_cmd(pattern="startvc$"))
+@bot.on(sudo_cmd(pattern="startvc$", allow_sudo=True))
 async def _(event):
     try:
-        await event.client(startvc(await get_call(event)))
-        await event.edit(event, "`Voice Chat Started...`")
-    except Exception as ex:
-        await event.edit(event, f"`{str(ex)}`")
+        await event.client(CreateGroupCallRequest(event.chat_id))
+        await eor(event, "**🔊 Voice Chat Started Successfully**")
+    except Exception as e:
+        await eor(event, f"`{str(e)}`")
 
 
-@register(outgoing=True, pattern=r"^\.stopvc (.*)")
+@bot.on(fire_on_cmd(pattern="endvc$"))
+@bot.on(sudo_cmd(pattern="endvc$", allow_sudo=True))
 async def _(event):
     try:
-        await event.client(stopvc(await get_call(event)))
-        await event.edit(event, "`Voice Chat Stopped...`")
-    except Exception as ex:
-        await event.edit(event, f"`{str(ex)}`")
+        await bot(DiscardGroupCallRequest(await getvc(event)))
+        await eor(event, "**📍 Voice Chat Ended Successfully !!**")
+    except Exception as e:
+        await eor(event, f"`{str(e)}`")
+
+
+@bot.on(fire_on_cmd(pattern="vcinvite$"))
+@bot.on(sudo_cmd(pattern="vcinvite$", allow_sudo=True))
+async def _(event):
+    hell = await eor(event, "`🧐 Inviting Users To Voice Chat....`")
+    users = []
+    i = 0
+    async for j in event.client.iter_participants(event.chat_id):
+        if not j.bot:
+            users.append(j.id)
+    hel_ = list(all_users(users, 6))
+    for k in hel_:
+        try:
+            await bot(InviteToGroupCallRequest(call=await getvc(event), users=k))
+            i += 6
+        except BaseException:
+            pass
+    await hell.edit(f"**🚀 Invited {i} Users to Voice Chat**")
