daaba765500550de3ee72b19158ab2c0073d9f2c
Chrisdroid1 <userbotni7@gmail.com>
2021-08-16 14:49:59 +0000

    SeD Replacer

--- firebot/modules/anime.py
+++ firebot/modules/anime.py
@@ -41,7 +41,7 @@ async def _(event):
     url5 = search[4].url
 
     await event.edit(
-        f"<b><u>Anime Search Complete</b></u> \n\n\n<b>Title</b>:-  <code>{title1}</code> \n<b>URL Link</b>:- {url1}\n\n<b>Title</b>:-  <code>{title2}</code> \n<b>URL Link</b>:- {url2}\n\n<b>Title</b>:-  <code>{title3}</code>\n<b>URL Link</b>:- {url3}\n\n<b>Title</b>:-  <code>{title4}</code> \n<b>URL Link</b>:- {url4}\n\n<b>Title</b>:-  <code>{title5}</code> \n<b>URL Link</b>:- {url5}\n\n<b>Links Gathered By Fire-X\nGet Your Own Fire-X From @FRIDAYCHAT</b>",
+        f"<b><u>Anime Search Complete</b></u> \n\n\n<b>Title</b>:-  <code>{title1}</code> \n<b>URL Link</b>:- {url1}\n\n<b>Title</b>:-  <code>{title2}</code> \n<b>URL Link</b>:- {url2}\n\n<b>Title</b>:-  <code>{title3}</code>\n<b>URL Link</b>:- {url3}\n\n<b>Title</b>:-  <code>{title4}</code> \n<b>URL Link</b>:- {url4}\n\n<b>Title</b>:-  <code>{title5}</code> \n<b>URL Link</b>:- {url5}\n\n<b>Links Gathered By Fire-X\nGet Your Own Fire-X From @FIREXUSERBOT</b>",
         parse_mode="HTML",
     )
     await ommhg.delete()
