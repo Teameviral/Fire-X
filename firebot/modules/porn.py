47b03ad92aed2c90d7ae61213ac6e629f20d4e02
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-08-17 08:16:54 +0100

    Update porn.py

--- firebot/modules/porn.py
+++ firebot/modules/porn.py
@@ -14,90 +14,90 @@ from ..utils import admin_cmd
 
 if pro == True:
 
-@borg.on(admin_cmd(pattern="porn"))
-async def _(event):  # @Black_lord_on_fire original
+    @borg.on(admin_cmd(pattern="porn"))
+    async def _(event):  # @Black_lord_on_fire original
 
-    if event.fwd_from:
+        if event.fwd_from:
 
-        return
+            return
 
-    animation_interval = 0.2
+        animation_interval = 0.2
 
-    animation_ttl = range(0, 8)
+        animation_ttl = range(0, 8)
 
-    await event.edit("`Connecting...`")
+        await event.edit("`Connecting...`")
 
-    animation_chars = [  # @Black_lord_on_fire original
-        "P_",
-        "PO_",
-        "POR_",
-        "PORN_",
-        "PORNH_",
-        "PORNHU_",
-        "PORNHUB_",
-        "[PORNHUB](www.porn93.cc)👄👅💦💦",  # @Black_lord_on_fire original
-    ]
+        animation_chars = [  # @Black_lord_on_fire original
+            "P_",
+            "PO_",
+            "POR_",
+            "PORN_",
+            "PORNH_",
+            "PORNHU_",
+            "PORNHUB_",
+            "[PORNHUB](www.porn93.cc)👄👅💦💦",  # @Black_lord_on_fire original
+        ]
 
-    for i in animation_ttl:
+        for i in animation_ttl:
 
-        await asyncio.sleep(animation_interval)
+            await asyncio.sleep(animation_interval)
 
-        await event.edit(animation_chars[i % 8])
+            await event.edit(animation_chars[i % 8])
 
-@borg.on(admin_cmd(pattern=r"xvideos"))
-async def _(event):
+    @borg.on(admin_cmd(pattern=r"xvideos"))
+    async def _(event):
 
-    if event.fwd_from:  # @Black_lord_on_fire original
+        if event.fwd_from:  # @Black_lord_on_fire original
 
-        return
+            return
 
-    animation_interval = 0.2
+        animation_interval = 0.2
 
-    animation_ttl = range(0, 7)
+        animation_ttl = range(0, 7)
 
-    await event.edit("`Connecting...`")
+        await event.edit("`Connecting...`")
 
-    animation_chars = [
-        "X_",
-        "XV_",
-        "XVI_",
-        "XVID_",
-        "XVIDE_",
-        "XVIDEO_",
-        "[XVIDEOS](www.xvideos4.com)🖕👄💦💦",
-    ]  # @Black_lord_on_fire original
+        animation_chars = [
+            "X_",
+            "XV_",
+            "XVI_",
+            "XVID_",
+            "XVIDE_",
+            "XVIDEO_",
+            "[XVIDEOS](www.xvideos4.com)🖕👄💦💦",
+        ]  # @Black_lord_on_fire original
 
-    for i in animation_ttl:
+        for i in animation_ttl:
 
-        await asyncio.sleep(animation_interval)
+            await asyncio.sleep(animation_interval)
 
-        await event.edit(animation_chars[i % 7])
+            await event.edit(animation_chars[i % 7])
 
-import asyncio
+    import asyncio
 
-@borg.on(admin_cmd(pattern=r"xnxx"))  # @Black_lord_on_fire original
-async def _(event):
+    @borg.on(admin_cmd(pattern=r"xnxx"))  # @Black_lord_on_fire original
+    async def _(event):
 
-    if event.fwd_from:
+        if event.fwd_from:
 
-        return
+            return
 
-    animation_interval = 0.2
+        animation_interval = 0.2
 
-    animation_ttl = range(0, 5)
+        animation_ttl = range(0, 5)
 
-    await event.edit("`Connecting...`")  # @Black_lord_on_fire original
+        await event.edit("`Connecting...`")  # @Black_lord_on_fire original
 
-    animation_chars = [
-        "X_",
-        "XN_",
-        "XNX_",
-        "XNXX_",
-        "[XNXX](www.xnxx.wapca.cc)👉🏻👌💦👄💦",
-    ]
+        animation_chars = [
+            "X_",
+            "XN_",
+            "XNX_",
+            "XNXX_",
+            "[XNXX](www.xnxx.wapca.cc)👉🏻👌💦👄💦",
+        ]
         # @Black_lord_on_fire original
-    for i in animation_ttl:
+        for i in animation_ttl:
 
-        await asyncio.sleep(animation_interval)
+            await asyncio.sleep(animation_interval)
 
-        await event.edit(animation_chars[i % 5])
+            await event.edit(animation_chars[i % 5])
