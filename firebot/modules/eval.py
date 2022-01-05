6595492dee0f7bde19b6f3def45e242322d45c96
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2022-01-05 20:55:08 +0530

    Update eval.py

--- firebot/modules/eval.py
+++ firebot/modules/eval.py
@@ -7,71 +7,85 @@ Syntax: .eval PythonCode"""
 import io
 import sys
 import traceback
+import time
+import asyncio
 
 from uniborg.util import edit_or_reply, fire_on_cmd, sudo_cmd
 
 from firebot import CMD_HELP
 
+EVAL = os.environ.get("EVAL", None)
+from . import *
 
 @fire.on(fire_on_cmd("eval"))
 async def _(event):
-    if event.fwd_from:
-        return
-    if event.sender.id in Config.SUDO_USERS:
-        return await event.reply("`Sorry kid but you are not worthy to use this command...`")
-    await edit_or_reply(event, "Processing ...")
-    cmd = event.text.split(" ", maxsplit=1)[1]
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-
-    old_stderr = sys.stderr
-    old_stdout = sys.stdout
-    redirected_output = sys.stdout = io.StringIO()
-    redirected_error = sys.stderr = io.StringIO()
-    stdout, stderr, exc = None, None, None
-
-    try:
-        await aexec(cmd, event)
-    except Exception:
-        exc = traceback.format_exc()
-
-    stdout = redirected_output.getvalue()
-    stderr = redirected_error.getvalue()
-    sys.stdout = old_stdout
-    sys.stderr = old_stderr
-
-    evaluation = ""
-    if exc:
-        evaluation = exc
-    elif stderr:
-        evaluation = stderr
-    elif stdout:
-        evaluation = stdout
-    else:
-        evaluation = "Success"
-
-    final_output = "**Eval**: `{}` \n\n **Output**: \n`{}` \n".format(cmd, evaluation)
-
-    if len(final_output) > Config.MAX_MESSAGE_SIZE_LIMIT:
-        with io.BytesIO(str.encode(final_output)) as out_file:
-            out_file.name = "eval.text"
-            await borg.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption=cmd,
-                reply_to=reply_to_id,
+if EVAL == "ON":
+        if event.fwd_from:
+            return
+        cmd = "".join(event.text.split(maxsplit=1)[1:])
+        if not cmd:
+            return await eor(event, "`What should i run ?..`")
+        eviralevent = await eor(event, "`Running ...`")
+        old_stderr = sys.stderr
+        old_stdout = sys.stdout
+        redirected_output = sys.stdout = io.StringIO()
+        redirected_error = sys.stderr = io.StringIO()
+        stdout, stderr, exc = None, None, None
+        try:
+            await aexec(cmd, event)
+        except Exception:
+            exc = traceback.format_exc()
+        stdout = redirected_output.getvalue()
+        stderr = redirected_error.getvalue()
+        sys.stdout = old_stdout
+        sys.stderr = old_stderr
+        evaluation = ""
+        if exc:
+            evaluation = exc
+        elif stderr:
+            evaluation = stderr
+        elif stdout:
+            evaluation = stdout
+        else:
+            evaluation = "Success"
+        final_output = f"•  Eval : \n`{cmd}` \n\n•  Result : \n`{evaluation}` \n"
+        # await eor(
+        # eviralevent,
+        # "**Eval Command Executed. Check out LOGGER_ID Group[Private Group Where All Message Forward]for result.**",
+        # )
+        if "session" in cmd:
+            await eor(
+                event, "String is a  Sensetive Data.\nSo, Its Protected By FIREX"
             )
-            await event.delete()
+            return
+        if "eviral_STRING" in cmd:
+            await eor(
+                event, "String is a  Sensetive Data.\nSo, Its Protected By FIREX"
+            )
+            return
+        else:
+            await eor(
+                eviralevent,
+                f"{final_output}",
+            )
     else:
-        await edit_or_reply(event, final_output)
+        await edit_or_reply(
+            event,
+            "If U Dont Know More About Then ask With Admin.\nTo Turn On ~  `.set var EVAL ON`",
+        )
 
 
-async def aexec(code, event):
-    exec(f"async def __aexec(event): " + "".join(f"\n {l}" for l in code.split("\n")))
-    return await locals()["__aexec"](event)
+async def aexec(code, smessatatus):
+    message = event = smessatatus
+    p = lambda _x: print(yaml_format(_x))
+    reply = await event.get_reply_message()
+    exec(
+        f"async def __aexec(message, event , reply, client, p, chat): "
+        + "".join(f"\n {l}" for l in code.split("\n"))
+    )
+    return await locals()["__aexec"](
+        message, event, reply, message.client, p, message.chat_id
+    )
 
 
 CMD_HELP.update(
