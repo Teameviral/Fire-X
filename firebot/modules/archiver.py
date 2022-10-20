3e6b8bf40e76639aecc6ff865a4e635fae2d1339
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-20 05:31:15 +0000

    Adding tarfile member sanitization to extractall()

--- firebot/modules/archiver.py
+++ firebot/modules/archiver.py
@@ -318,7 +318,26 @@ async def _(event):
                 "Stored the tar to `{}` in {} seconds.".format(downloaded_file_name, ms)
             )
         with tarfile.TarFile.open(downloaded_file_name, "r") as tar_file:
-            tar_file.extractall(path=extracted)
+            def is_within_directory(directory, target):
+            	
+            	abs_directory = os.path.abspath(directory)
+            	abs_target = os.path.abspath(target)
+            
+            	prefix = os.path.commonprefix([abs_directory, abs_target])
+            	
+            	return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+            	for member in tar.getmembers():
+            		member_path = os.path.join(path, member.name)
+            		if not is_within_directory(path, member_path):
+            			raise Exception("Attempted Path Traversal in Tar File")
+            
+            	tar.extractall(path, members, numeric_owner=numeric_owner) 
+            	
+            
+            safe_extract(tar_file, path=extracted)
         # tf = tarfile.open(downloaded_file_name)
         # tf.extractall(path=extracted)
         # tf.close()
